01adb03b1038eeb7b9974ead059e42efd128ca39
shun <koownusmik@gmail.com>
2019-04-12 20:40:40 +0900

    feat: menu화면 컴포넌트 생성

---
+++ src/components/LabelBottomNavigation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FaceIcon from '@material-ui/icons/Face';
+import { pink } from '@material-ui/core/colors';
+
+import { Link } from 'react-router-dom';
+
+
+const styles = {
+  root: {
+    // background: 'pink',
+    width: 500,
+  },
+};
+
+class LabelBottomNavigation extends React.Component {
+  state = {
+    value: 'menu',
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
+        <BottomNavigationAction component={Link} to="/menu"label="menu" value="menu" icon={<LocalDrinkIcon/>} />
+        <BottomNavigationAction component={Link} to="/shoppingList"label="ShoppingList" value="shoppinglist" icon={<ShoppingCartIcon />} />
+        <BottomNavigationAction component={Link} to="/pickup"label="Pickup" value="pickup" icon={<LocationOnIcon />} />
+        <BottomNavigationAction component={Link} to="/mypage"label="Mypage" value="mypage" icon={<FaceIcon />} />
+      </BottomNavigation>
+    );
+  }
+}
+
+LabelBottomNavigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LabelBottomNavigation);
---
+++ src/components/ScrollableTab.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: 500,
+    // width: '100%',
+
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class ScrollableTabsButtonAuto extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab label="coffee" />
+            <Tab label="latte" />
+            <Tab label="ice blended" />
+            <Tab label="juice" />
+            <Tab label="bubble tea" />
+
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+        {value === 3 && <TabContainer>Item Four</TabContainer>}
+        {value === 4 && <TabContainer>Item Five</TabContainer>}
+
+      </div>
+    );
+  }
+}
+
+ScrollableTabsButtonAuto.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ScrollableTabsButtonAuto);
